b05728099c2150d79aa650b799dd4074c62b2743
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-11 15:49:25 +0200

    add teams array

--- snack_02/script.js
+++ snack_02/script.js
@@ -1,2 +1,38 @@
 console.log('linked');
 
+/*Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
+
+
+
+// Creiamo un array di squadre di calcio con solo i nomi
+let squadre = [
+    
+    {
+        nome: 'squadra_1',
+        punti: 0,
+        falli: 0
+    },
+
+    {
+        nome: 'squadra_2',
+        punti: 0,
+        falli: 0
+    },
+
+    {
+        nome: 'squadra_2',
+        punti: 0,
+        falli: 0
+    },
+
+    {
+        nome: 'squadra_3',
+        punti: 0,
+        falli: 0
+    }
+];
+
