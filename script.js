7c17a2b2e841f036f72e2479b24953dabd4b8924
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-11 15:14:31 +0200

    add bikes  object array

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 console.log('linked');
 
+const bikes = [
+    {
+        nome: 'bike1',
+        peso: 10,
+    },
+    {
+        nome: 'bike2',
+        peso: 20,
+    },
+    {
+        nome: 'bike3',
+        peso: 30,
+    },
+    {
+        nome: 'bike4',
+        peso: 40,
+    },
+]
+
